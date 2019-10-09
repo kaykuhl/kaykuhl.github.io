bbf2d7435455dffb5fd90947ed305d46eed99f39
Kayla Kuhlman <kakoreo4@gmail.com>
2019-10-09 00:10:00 -0500

    Updated js file with buttons for 1st question. Needs work

--- homework-4/script.js
+++ homework-4/script.js
@@ -1,40 +1,73 @@
 var startQuizButton = document.querySelector("#startQuiz");
-var timer = document.querySelector("#seconds")
-var quizQuestionsDiv = document.querySelector("#quizQuestions")
+var startButtonDiv = document.querySelector("#startButton")
+var timer = document.querySelector("#seconds");
+var quizQuestionsDiv = document.querySelector("#quizQuestions");
+var quizChoicesDiv = document.querySelector("#quizChoices");
+var selectionA = document.querySelector("#A")
+var selectionB = document.querySelector("#B")
+var selectionC = document.querySelector("#C")
+var selectionD = document.querySelector("#D")
+var isCorrect = true
 
-time = 60
+var time = 60
 
 function startTimer() {
-  
-    interval = setInterval(function() {
-      time--;
-      timer.textContent = time
-    }, 1000);
-  }
 
-function quiz () {
-    
-    startTimer()
+  interval = setInterval(function () {
+    time--;
+    timer.textContent = time
+  }, 1000);
+}
 
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+];
 
-  ];
-//   var questionString = JSON.stringify(questions)
-//   quizQuestionsDiv.textContent = questionString
-quizQuestionsDiv.textContent = Object.values(questions)
+function quiz() {
+  //removes start quiz button
+  startButtonDiv.removeChild(startQuizButton);
+
+  //starts timer
+  startTimer()
+  quizQuestionsDiv.innerHTML = questions[0].title
+
+  //For loop to create buttons and append to selection divs
+  for (i = 0; i < 4; i++) {
+    var buttons = document.createElement("button");
+    buttons.innerHTML = questions[0].choices[i];
+    quizChoicesDiv.children[i].append(buttons)
+    buttons.className = "btn btn-danger btn-xs"
+  }
+
+// Button Selections (User Choice)
+  selectionA.addEventListener("click", function () {
+    if (questions[0].choices[0] === questions[0].answer) { alert("you are correct!") }
+    else { alert("you are wrong!")}
+  })
+  selectionB.addEventListener("click", function () {
+    if (questions[0].choices[1] === questions[0].answer) { alert("you are correct!") }
+    else { alert("you are wrong!")}
+  })
+  selectionC.addEventListener("click", function () {
+    if (questions[0].choices[2] === questions[0].answer) { alert("you are correct!") }
+    else { alert("you are wrong!")}
+  })
+  selectionD.addEventListener("click", function () {
+    if (questions[0].choices[3] === questions[0].answer) { alert("you are correct!") }
+    else { alert("you are wrong!")}
+  })
+
 
 }
 
 
-
-  startQuizButton.addEventListener("click", quiz);
+startQuizButton.addEventListener("click", quiz);
