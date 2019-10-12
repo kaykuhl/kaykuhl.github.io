b2e72b5e53b6b23c37b23c815d2d7ceb53c8fe88
Kayla Kuhlman <kakoreo4@gmail.com>
2019-10-12 01:27:54 -0500

    fixed quiz js. added background and separated questions into seperate file

---
+++ homework-4/quiz-questions.js
@@ -0,0 +1,57 @@
+var questions = [
+    {
+      title: "Where was the world's first pizzeria opened in 1830?",
+      choices: ["Naples, Italy", "Rome, Italy", "Venice, Italy", "Florance, Italy"],
+      answer: "Naples, Italy"
+    },
+    {
+      title: "How many slices of pizza does an average person eat in america?",
+      choices: ["121 slices", "12 slices", "20 slices", "46 slices"],
+      answer: "Crainium"
+    },
+    {
+      title: "What is the most popular pizza topping",
+      choices: ["Extra Cheese", "Pepperoni", "Sausage", "Mushroom"],
+      answer: "Pepperoni"
+    },
+    {
+      title: "When did Dominos introduce their online pizza tracker?",
+      choices: ["2013", "2006", "2011", "2008"],
+      answer: "2008"
+    },
+    {
+      title: "When the moon hits your eye like a big pizza pie, thats... _____________",
+      choices: ["Adore'", "Amore'", "Agore'", "Alore'"],
+      answer: "Amore'"
+    },
+    {
+      title: "Where does Minneapolis rank on top US cities for most resturants serving pizza per capita",
+      choices: ["3rd", "100th", "11th", "55th"],
+      answer: "3rd"
+    },
+    {
+      title: "What pizza chain's slogan is: Better Ingredients... Better Pizza?",
+      choices: ["Pizza Hut", "Godfather's", "Little Caesar's", "Papa John's"],
+      answer: "Papa John's"
+    },
+    {
+      title: "What is on a traditional margarita pizza?",
+      choices: ["mozzarella cheese, fresh tomatoes, basil", "tequila, lime, salt", "pepperoni, mozzarella cheese, sauce", "olive oil, sauce, mozzarella cheese, oregano"],
+      answer: "mozzarella cheese, fresh tomatoes, basil"
+    },
+    {
+      title: "Which pizza chain was the first to offer stuffed-crust nationwide?",
+      choices: ["Papa John's", "Pizza Hut", "Domino's", "UNO"],
+      answer: "Pizza Hut"
+    },
+    {
+      title: "What is America's least favorite pizza topping?",
+      choices: ["Mushrooms", "Anchovies", "Pineapple", "Jalapenos"],
+      answer: "Anchovies"
+    },
+    {
+      title: "What is is the best food?",
+      choices: ["Pizza", "Pizza", "Pizza", "Pizza"],
+      answer: "Pizza"
+    },
+  ];
--- homework-4/script.js
+++ homework-4/script.js
@@ -1,3 +1,6 @@
+// Note: Quiz questions are stored in seperate js file (quiz-questions.js) and referenced in HTML
+
+//Global variables
 var startQuizButton = document.querySelector("#startQuiz");
 var startButtonDiv = document.querySelector("#startButton")
 var timer = document.querySelector("#seconds");
@@ -7,10 +10,20 @@ var selectionA = document.querySelector("#A")
 var selectionB = document.querySelector("#B")
 var selectionC = document.querySelector("#C")
 var selectionD = document.querySelector("#D")
+var resultDiv = document.querySelector("#result")
+var buttonsEl = document.querySelector(".btn")
 var isCorrect = true
+var input = document.getElementById("saveScore");
 
+
+//Set userScore and current question to 0
+var userScore = 0
+var currentQuestion = 0
+
+//Set start time
 var time = 60
 
+//function to start timer
 function startTimer() {
 
   interval = setInterval(function () {
@@ -19,55 +32,149 @@ function startTimer() {
   }, 1000);
 }
 
-var questions = [
-  {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
-  },
-];
-
-function quiz() {
-  //removes start quiz button
-  startButtonDiv.removeChild(startQuizButton);
-
-  //starts timer
+//function to start quiz
+function startQuiz() {
   startTimer()
-  quizQuestionsDiv.innerHTML = questions[0].title
+  //removes start quiz button and inner HTML
+  startButtonDiv.innerHTML = ""
+
+  //adds question title to HTML
+  quizQuestionsDiv.innerHTML = questions[currentQuestion].title
 
   //For loop to create buttons and append to selection divs
   for (i = 0; i < 4; i++) {
     var buttons = document.createElement("button");
-    buttons.innerHTML = questions[0].choices[i];
-    quizChoicesDiv.children[i].append(buttons)
-    buttons.className = "btn btn-danger btn-xs"
+    buttons.innerHTML = questions[currentQuestion].choices[i];
+    quizChoicesDiv.children[i].append(buttons);
+    buttons.className = "btn btn-outline-dark btn-lg btn-block";
   }
+}
 
-// Button Selections (User Choice)
-  selectionA.addEventListener("click", function () {
-    if (questions[0].choices[0] === questions[0].answer) { alert("you are correct!") }
-    else { alert("you are wrong!")}
-  })
-  selectionB.addEventListener("click", function () {
-    if (questions[0].choices[1] === questions[0].answer) { alert("you are correct!") }
-    else { alert("you are wrong!")}
-  })
-  selectionC.addEventListener("click", function () {
-    if (questions[0].choices[2] === questions[0].answer) { alert("you are correct!") }
-    else { alert("you are wrong!")}
-  })
-  selectionD.addEventListener("click", function () {
-    if (questions[0].choices[3] === questions[0].answer) { alert("you are correct!") }
-    else { alert("you are wrong!")}
+// If else for Button Selections (User Choice) comparison to answer values
+selectionA.addEventListener("click", function () {
+  if (questions[currentQuestion].choices[0] === questions[currentQuestion].answer) {
+    isCorrect = true;
+    displayResult();
+  }
+  else {
+    isCorrect = false;
+    displayResult();
+  }
+})
+
+selectionB.addEventListener("click", function () {
+  if (questions[currentQuestion].choices[1] === questions[currentQuestion].answer) {
+    isCorrect = true;
+    displayResult();
+  }
+  else {
+    isCorrect = false;
+    displayResult();
+  }
+})
+
+selectionC.addEventListener("click", function () {
+  if (questions[currentQuestion].choices[2] === questions[currentQuestion].answer) {
+    isCorrect = true;
+    displayResult();
+  }
+  else {
+    isCorrect = false;
+    displayResult();
+  }
+})
+
+selectionD.addEventListener("click", function () {
+  if (questions[currentQuestion].choices[3] === questions[currentQuestion].answer) {
+    isCorrect = true;
+    displayResult();
+  }
+  else {
+    isCorrect = false;
+    displayResult();
+  }
+})
+
+// function to display results
+function displayResult() {
+  if (isCorrect === true) {
+    userScore = userScore + 10;
+    resultDiv.innerHTML = "<hr>" + "You are CORRECT!!! You have " + userScore + " points";
+    increaseQuestion();
+  }
+  else {
+    time = time - 10;
+    resultDiv.innerHTML = "<hr>" + "You are WRONG!!! You have " + userScore + " points";;
+    increaseQuestion();
+  }
+}
+
+//function to go onto next question
+function increaseQuestion() {
+
+  currentQuestion++;
+
+  //End Quiz conditions
+  if (currentQuestion === 10 || time < 0) {
+    clearInterval(interval)
+    quizQuestionsDiv.innerHTML = "Quiz is Complete!"
+    quizChoicesDiv.innerHTML = "Congratulations, you have " + userScore + " points!" + "<br>"
+    resultDiv.innerHTML = ""
+    timer.innerHTML = ""
+    highScore()
+
+  }
+  else {
+    renderNextQuestion();
+  }
+}
+
+//Submit high score
+function highScore() {
+  var newForm = document.createElement("form");
+  resultDiv.appendChild(newForm);
+
+  newForm.setAttribute('method', "post");
+  newForm.setAttribute('action', "submit.php");
+  var input = document.createElement("input");
+  input.setAttribute('type', "text");
+  input.setAttribute('name', "initals");
+  input.className = "inputInitials"
+
+  var submit = document.createElement("input");
+  submit.setAttribute('type', "submit");
+  submit.setAttribute('value', "Submit");
+  submit.className = "submitButton"
+
+  newForm.appendChild(input);
+  newForm.appendChild(submit);
+
+
+  newForm.addEventListener("submit", function () {
+    event.preventDefault();
+    var inputEl = document.querySelector(".inputInitials")
+    var result = { initals: inputEl, score: userScore }
+    localStorage.setItem( 'highScore', JSON.stringify(result) );
+    console.log( JSON.parse( localStorage.getItem( 'highScore' ) ) );
+
   })
 
 
 }
 
+// Retrieve
+var score = localStorage.getItem("score");
 
-startQuizButton.addEventListener("click", quiz);
+//Function to render next questions
+function renderNextQuestion() {
+  quizQuestionsDiv.innerHTML = questions[currentQuestion].title;
+
+  //changing inner HTML of buttons (tried to do this with for loop and failed)  
+  selectionA.childNodes[0].innerHTML = questions[currentQuestion].choices[0];
+  selectionB.childNodes[0].innerHTML = questions[currentQuestion].choices[1];
+  selectionC.childNodes[0].innerHTML = questions[currentQuestion].choices[2];
+  selectionD.childNodes[0].innerHTML = questions[currentQuestion].choices[3];
+}
+
+//Button event to start quiz
+startQuizButton.addEventListener("click", startQuiz);
