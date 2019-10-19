1fe296b0d8ccb901afbb89c63991fd85d8629f50
Kayla Kuhlman <kakoreo4@gmail.com>
2019-10-19 16:51:01 -0500

    read me and javascript updated

--- homework-5/README.md
+++ homework-5/README.md
@@ -1,87 +1,32 @@
-# Unit 05 Third-Party APIs Homework: Day Planner
+# Daily Planner
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+## Description
+This application is an interactive daily planner and was created using javaScript and Moment.js 
+Link to Application [here](https://kaykuhl.github.io/homework-5/)
 
-The app should display standard business hours (9 a.m. to 5 p.m.). Each time slot should represent one hour and contain the following:
+## Features
+The application has time slots for each of the hours between 9AM and 5PM.  In each of the time slots, there is the following:
+*Time
+*A textarea for user's to input text (for example, a task to be completed or meeting to attend during that hour)
+*A save button
 
-* The time
+The save button will save the user inputs to local storage so that even when the page is refreshed, the data is saved in the browser.
 
-* A field to hold user input
+The current day and time is displayed at the top of the calendar and additionally, based on the time of day, the hour blocks will change color to indicate past, present, or future hour fields.
 
-* A save button
 
-Clicking on the save button will store the time and user input in `localStorage`.
+## Screenshots
 
-Near the top of the calendar, the application should display the current day. Additionally, each hour should be color coded to reflect whether the time slot is in the past, the present, or the future. This will change depending on the time of day.
+The current day and time is displayed on top of the page.
+![Landing Page Screenshot](https://kaykuhl.github.io/homework-5/images/readme-index.PNG)
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+Based on the current time of day, the color of the time blocks will reflect if the hour is in the past, present, or future.
+![Colors Screenshot](https://kaykuhl.github.io/homework-5/images/readme-colors.PNG)
 
-![day planner demo](./Assets/05-Third-Party-APIs-homework-demo.gif)
+The user is able to type in input next to each hour and by pressing the save button, pushing the input to local storage in the browser.
+![Saving Screenshot](https://kaykuhl.github.io/homework-5/images/readme-save.PNG)
 
-## User Story
 
-AS AN employee with a busy schedule
 
-I WANT to add important events to a daily planner
 
-SO THAT I can manage my time effectively 
 
-## Business Context
-
-Poor time management can result in missed meetings and deadlines or create the appearance of unprofessionalism. A daily planner allows employees to see their day at a glance, schedule time effectively, and improve productivity. 
-
-## Minimum Requirements
-
-* Functional, deployed application.
-
-* GitHub repository with a unique name and a README describing project.
-
-* The application displays timeblocks for standard business hours (9 a.m. to 5 p.m.).
-
-* Each timeblock contains an input field and save button.
-
-* Clicking a timeblock's "Save" button stores the input text in local storage, allowing the text to persist when the application is refreshed.
-
-* The current day is displayed at the top of the calendar.
-
-* Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
-
-```
-GIVEN that an employee adds events to a specific hour in a calendar
-
-WHEN the employee clicks the save button
-
-THEN events are saved in the timeblock for that hour
-```
-- - -
-
-## Commit Early and Often
-
-One of the most important skills to master as a web developer is version control. Building the habit of committing via Git is important for two reasons:
-
-* Your commit history is a signal to employers that you are actively working on projects and learning new skills.
-
-* Your commit history allows you to revert your codebase in the event that you need to return to a previous state.
-
-Follow these guidelines for committing:
-
-* Make single-purpose commits for related changes to ensure a clean, manageable history. If you are fixing two issues, make two commits.
-
-* Write descriptive, meaningful commit messages so that you and anyone else looking at your repository can easily understand its history.
-
-* Don't commit half-done work, for the sake of your collaborators (and your future self!).
-
-* Test your application before you commit to ensure functionality at every step in the development process.
-
-We would like you to have well over 200 commits by graduation, so commit early and often!
-
-## Submission on BCS
-
-You are required to submit the following:
-
-* The URL of the deployed application
-
-* The URL of the GitHub repository
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- homework-5/script.js
+++ homework-5/script.js
@@ -1,23 +1,22 @@
-var hour9 = moment("9:00:00 am", "h:mm:ss a").format("h a")
-var hour10 = moment("10:00:00 am", "h:mm:ss a").format("h a")
-var hour11 = moment("11:00:00 am", "h:mm:ss a").format("h a")
-var hour12 = moment("12:00:00 pm", "h:mm:ss a").format("h a")
-var hour1 = moment("1:00:00 pm", "h:mm:ss a").format("h a")
-var hour2 = moment("2:00:00 pm", "h:mm:ss a").format("h a")
-var hour3 = moment("3:00:00 pm", "h:mm:ss a").format("h a")
-var hour4 = moment("4:00:00 am", "h:mm:ss a").format("h a")
-var hour5 = moment("5:00:00 pm", "h:mm:ss a").format("h a")
+// Variables for each hour
+var hour9 = moment("9:00:00 am", "h:mm:ss a")
+var hour10 = moment("10:00:00 am", "h:mm:ss a")
+var hour11 = moment("11:00:00 am", "h:mm:ss a")
+var hour12 = moment("12:00:00 pm", "h:mm:ss a")
+var hour1 = moment("1:00:00 pm", "h:mm:ss a")
+var hour2 = moment("2:00:00 pm", "h:mm:ss a")
+var hour3 = moment("3:00:00 pm", "h:mm:ss a")
+var hour4 = moment("4:00:00 am", "h:mm:ss a")
+var hour5 = moment("5:00:00 pm", "h:mm:ss a")
+var hour6 = moment("6:00:00 pm", "h:mm:ss a")
 
-var time9Div = $("#time9")
-var time10Div = $("#time10")
-var time11Div = $("#time11")
-var time12Div = $("#time12")
-var time1Div = $("#time1")
-var time2Div = $("#time2")
-var time3Div = $("#time3")
-var time4Div = $("#time4")
-var time5Div = $("#time5")
+// Variable for time now
+var time = moment()
 
+// Pushes current date and time onto HTML
+$("#currentDay").text(time.format("LLL"))
+
+// Variables for text box input
 var text9Div = $("#text9")
 var text10Div = $("#text10")
 var text11Div = $("#text11")
@@ -28,16 +27,34 @@ var text3Div = $("#text3")
 var text4Div = $("#text4")
 var text5Div = $("#text5")
 
-time9Div.text(hour9)
-time10Div.text(hour10)
-time11Div.text(hour11)
-time12Div.text(hour12)
-time1Div.text(hour1)
-time2Div.text(hour2)
-time3Div.text(hour3)
-time4Div.text(hour4)
-time5Div.text(hour5)
 
+// Pulling input from local storage when document is ready
+$(document).ready(function() {
+text9Div.val(localStorage.getItem("9AM"));
+text10Div.val(localStorage.getItem("10AM"));
+text11Div.val(localStorage.getItem("11AM"));
+text12Div.val(localStorage.getItem("12PM"));
+text1Div.val(localStorage.getItem("1PM"));
+text2Div.val(localStorage.getItem("2PM"));
+text3Div.val(localStorage.getItem("3PM"));
+text4Div.val(localStorage.getItem("4PM"));
+text5Div.val(localStorage.getItem("5PM"));
+})
+
+// Function to save input into local storage
+function savePlanner() {
+    localStorage.setItem("9AM", text9Div.val());
+    localStorage.setItem("10AM", text10Div.val());
+    localStorage.setItem("11AM", text11Div.val());
+    localStorage.setItem("12PM", text12Div.val());
+    localStorage.setItem("1PM", text1Div.val());
+    localStorage.setItem("2PM", text2Div.val());
+    localStorage.setItem("3PM", text3Div.val());
+    localStorage.setItem("4PM", text4Div.val());
+    localStorage.setItem("5PM", text5Div.val());
+}
+
+// Variables for individual buttons
 var button0 = document.getElementById("button0")
 var button1 = document.getElementById("button1")
 var button2 = document.getElementById("button2")
@@ -48,29 +65,7 @@ var button6 = document.getElementById("button6")
 var button7 = document.getElementById("button7")
 var button8 = document.getElementById("button8")
 
-text9Div.val(localStorage.getItem("9AM"));
-text9Div.val(localStorage.getItem("10AM"));
-text9Div.val(localStorage.getItem("11AM"));
-text9Div.val(localStorage.getItem("12PM"));
-text9Div.val(localStorage.getItem("1PM"));
-text9Div.val(localStorage.getItem("2PM"));
-text9Div.val(localStorage.getItem("3PM"));
-text9Div.val(localStorage.getItem("4PM"));
-text9Div.val(localStorage.getItem("5PM"));
-
-function savePlanner() {
-    localStorage.setItem("9AM", text9Div.val());
-    localStorage.setItem("10AM", text9Div.val());
-    localStorage.setItem("11AM", text9Div.val());
-    localStorage.setItem("12PM", text9Div.val());
-    localStorage.setItem("1PM", text9Div.val());
-    localStorage.setItem("2PM", text9Div.val());
-    localStorage.setItem("3PM", text9Div.val());
-    localStorage.setItem("4PM", text9Div.val());
-    localStorage.setItem("5PM", text9Div.val());
-
-}
-
+// Button On Click Functions (Runs savePlanner Function)
 button0.addEventListener("click", savePlanner);
 button1.addEventListener("click", savePlanner);
 button2.addEventListener("click", savePlanner);
@@ -81,21 +76,103 @@ button6.addEventListener("click", savePlanner);
 button7.addEventListener("click", savePlanner);
 button8.addEventListener("click", savePlanner);
 
-// $(".div9").setAttribute("class","future row time-block")
-// $(".div10").setAttribute("class","future row time-block")
-// $(".div11").setAttribute("class","future row time-block")
-// $(".div12").setAttribute("class","future row time-block")
-// $(".div1").setAttribute("class","future row time-block")
-// $(".div2").setAttribute("class","future row time-block")
-// $(".div3").setAttribute("class","future row time-block")
-// $(".div4").setAttribute("class","future row time-block")
-// $(".div5").setAttribute("class","future row time-block")
+// Sets all the Time Slots to "Future"
+$(".div9").attr("class", "future col-md-10 div9")
+$(".div10").attr("class", "future col-md-10 div10")
+$(".div11").attr("class", "future col-md-10 div11")
+$(".div12").attr("class", "future col-md-10 div12")
+$(".div1").attr("class", "future col-md-10 div1")
+$(".div2").attr("class", "future col-md-10 div2")
+$(".div3").attr("class", "future col-md-10 div3")
+$(".div4").attr("class", "future col-md-10 div4")
+$(".div5").attr("class", "future col-md-10 div5")
 
 
-// if (moment().isBefore("6:00:00 pm", "h:mm:ss a") === true) {
-//     text9Div.className = "past"
-// }
-// else {
-    
+// If statements to change div classes based on if time is between X and Y
+// If time is between 9AM and 10AM
+if (time.isBetween(hour9, hour10)) {
+    $(".div9").attr("class", "present col-md-10 div9")
+}
+// If time is between 10AM and 11AM
+if (time.isBetween(hour10, hour11)) {
+    $(".div9").attr("class", "past col-md-10 div9")
+    $(".div10").attr("class", "present col-md-10 div10")
+}
+// If time is between 11AM and 12PM
+if (time.isBetween(hour11, hour12)) {
+    $(".div9").attr("class", "past col-md-10 div9")
+    $(".div10").attr("class", "past col-md-10 div10")
+    $(".div11").attr("class", "present col-md-10 div11")
+}
+// If time is between 12PM and 1PM
+if (time.isBetween(hour12, hour1)) {
+    $(".div9").attr("class", "past col-md-10 div9")
+    $(".div10").attr("class", "past col-md-10 div10")
+    $(".div11").attr("class", "past col-md-10 div11")
+    $(".div12").attr("class", "present col-md-10 div12")
+}
+// If time is between 1PM and 2PM
+if (time.isBetween(hour1, hour2)) {
+    $(".div9").attr("class", "past col-md-10 div9")
+    $(".div10").attr("class", "past col-md-10 div10")
+    $(".div11").attr("class", "past col-md-10 div11")
+    $(".div12").attr("class", "past col-md-10 div12")
+    $(".div1").attr("class", "present col-md-10 div1")
+}
+// If time is between 2PM and 3PM
+if (time.isBetween(hour2, hour3)) {
+    $(".div9").attr("class", "past col-md-10 div9")
+    $(".div10").attr("class", "past col-md-10 div10")
+    $(".div11").attr("class", "past col-md-10 div11")
+    $(".div12").attr("class", "past col-md-10 div12")
+    $(".div1").attr("class", "past col-md-10 div1")
+    $(".div2").attr("class", "present col-md-10 div2")
+}
+// If time is between 3PM and 4PM
+if (time.isBetween(hour3, hour4)) {
+    $(".div9").attr("class", "past col-md-10 div9")
+    $(".div10").attr("class", "past col-md-10 div10")
+    $(".div11").attr("class", "past col-md-10 div11")
+    $(".div12").attr("class", "past col-md-10 div12")
+    $(".div1").attr("class", "past col-md-10 div1")
+    $(".div2").attr("class", "past col-md-10 div2")
+    $(".div3").attr("class", "present col-md-10 div3")
+}
+// If time is between 4PM and 5PM
+if (time.isBetween(hour4, hour5)) {
+    $(".div9").attr("class", "past col-md-10 div9")
+    $(".div10").attr("class", "past col-md-10 div10")
+    $(".div11").attr("class", "past col-md-10 div11")
+    $(".div12").attr("class", "past col-md-10 div12")
+    $(".div1").attr("class", "past col-md-10 div1")
+    $(".div2").attr("class", "past col-md-10 div2")
+    $(".div3").attr("class", "past col-md-10 div3")
+    $(".div4").attr("class", "present col-md-10 div4")
+}
+// If time is between 5PM and 6PM
+if (time.isBetween(hour5, hour6)) {
+    $(".div9").attr("class", "past col-md-10 div9")
+    $(".div10").attr("class", "past col-md-10 div10")
+    $(".div11").attr("class", "past col-md-10 div11")
+    $(".div12").attr("class", "past col-md-10 div12")
+    $(".div1").attr("class", "past col-md-10 div1")
+    $(".div2").attr("class", "past col-md-10 div2")
+    $(".div3").attr("class", "past col-md-10 div3")
+    $(".div4").attr("class", "past col-md-10 div4")
+    $(".div5").attr("class", "present col-md-10 div5")
+}
+// If time is after 6PM
+if (time.isAfter(hour6)) {
+    $(".div9").attr("class", "past col-md-10 div9")
+    $(".div10").attr("class", "past col-md-10 div10")
+    $(".div11").attr("class", "past col-md-10 div11")
+    $(".div12").attr("class", "past col-md-10 div12")
+    $(".div1").attr("class", "past col-md-10 div1")
+    $(".div2").attr("class", "past col-md-10 div2")
+    $(".div3").attr("class", "past col-md-10 div3")
+    $(".div4").attr("class", "past col-md-10 div4")
+    $(".div5").attr("class", "past col-md-10 div5")
+    $(".div5").attr("class", "past col-md-10 div2")
+}
 
-// }
+
