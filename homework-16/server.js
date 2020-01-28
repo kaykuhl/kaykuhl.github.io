21d3963864ee59fa3b1dc0064aacf88ae230d081
ThelegendofKayla <kakoreo4@gmail.com>
2020-01-27 19:43:09 -0600

    moved file structure, updated server file

---
+++ homework-16/server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/api")(app);
+
+app.use(express.static("public"));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", {
+  useNewUrlParser: true
+});
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
