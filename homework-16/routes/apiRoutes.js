904c030309e51120f0a7d7b089ff9e230467245f
Kayla Kuhlman <kakoreo4@gmail.com>
2020-01-29 20:43:35 -0600

    updated routes but still need to fix application

--- homework-16/routes/apiRoutes.js
+++ homework-16/routes/apiRoutes.js
@@ -8,16 +8,40 @@ module.exports = function(app) {
   });
 
   app.put("/api/workouts/:id", function(req, res) {
-    db.Workout.updateOne({ _id: req.params.id }).then(function(result) {
+    db.Workout.update({ _id: req.params.id }, {
+      "type": req.body.type,
+      "name": req.body.name,
+      "distance": req.body.distance,
+      "duration": req.body.duration,
+      "weight": req.body.weight,
+      "sets": req.body.sets,
+      "reps": req.body.reps,
+    }).then(function(result) {
       res.json(result);
     });
   });
 
-
-  app.put("/api/workouts/range", function(req, res) {
-    // db.Workout.updateOne({ _id: req.params.id }).then(function(result) {
-    //   res.json(result);
+  app.get("/api/workouts/range", ({ body }, res) => {
+    db.Workout.find({})
+      .then(dbTransaction => {
+    db.Workout.find({}).then(function(result) {
+      res.json(result);
     });
   });
-
+  })
+  
+  app.post("/api/workouts/", function(req, res) {
+    db.Workout.create({ _id: req.params.id }, {
+      "type": req.body.type,
+      "name": req.body.name,
+      "distance": req.body.distance,
+      "duration": req.body.duration,
+      "weight": req.body.weight,
+      "sets": req.body.sets,
+      "reps": req.body.reps,
+    }).then(function(result) {
+      res.json(result);
+    });
+  })
+  
 };
