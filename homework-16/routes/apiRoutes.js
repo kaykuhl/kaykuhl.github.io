d51aaf26d173fdf5b02a2a2f9a1ca79399a1bba3
Kayla Kuhlman <kakoreo4@gmail.com>
2020-01-31 20:44:36 -0600

    udpated files with newer push from chris, made some changes to the routes but still not working

--- homework-16/routes/apiRoutes.js
+++ homework-16/routes/apiRoutes.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
   });
 
   app.put("/api/workouts/:id", function(req, res) {
-    db.Workout.update({ _id: req.params.id }, {
+    db.Workout.updateOne({ _id: req.params.id }, {
       "type": req.body.type,
       "name": req.body.name,
       "distance": req.body.distance,
@@ -21,24 +21,22 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/api/workouts/range", ({ body }, res) => {
-    db.Workout.find({})
-      .then(dbTransaction => {
+  app.get("/api/workouts/range", function(req, res) {
     db.Workout.find({}).then(function(result) {
-      res.json(result);
+        res.json(result);
     });
-  });
-  })
+});
   
   app.post("/api/workouts/", function(req, res) {
-    db.Workout.create({ _id: req.params.id }, {
-      "type": req.body.type,
-      "name": req.body.name,
-      "distance": req.body.distance,
-      "duration": req.body.duration,
-      "weight": req.body.weight,
-      "sets": req.body.sets,
-      "reps": req.body.reps,
+    db.Workout.create({
+      day: new Date().setDate(new Date().getDate()),
+      type: req.body.type,
+      name: req.body.name,
+      distance: req.body.distance,
+      duration: req.body.duration,
+      weight: req.body.weight,
+      sets: req.body.sets,
+      reps: req.body.reps,
     }).then(function(result) {
       res.json(result);
     });
