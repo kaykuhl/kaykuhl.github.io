2c340a99a7b0c2f3e6ae7326d7bc27c92e4e015d
ThelegendofKayla <kakoreo4@gmail.com>
2020-02-17 14:21:07 -0600

    created components for About, contact, and Home and linked to them on nav bar. Fixed img source and JSX language as well as linked to CSS

--- homework-19/src/App.js
+++ homework-19/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import CarContainer from "./components/CarContainer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 function App() {
-  return <CarContainer />;
+  return (
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
