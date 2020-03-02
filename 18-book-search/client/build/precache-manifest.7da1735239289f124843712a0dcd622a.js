ef62cbcac1068eeb5ae22a8499f53ff4c3443e38
Kayla Kuhlman <kakoreo4@gmail.com>
2020-03-02 17:30:22 -0600

    working app

---
+++ 18-book-search/client/build/precache-manifest.7da1735239289f124843712a0dcd622a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bd8f7c03e06131ee890",
+    "url": "/static/css/main.3e5a3305.chunk.css"
+  },
+  {
+    "revision": "0bd8f7c03e06131ee890",
+    "url": "/static/js/main.6b8991a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4535c77ce49c38f7009",
+    "url": "/static/js/2.6f194c4f.chunk.js"
+  },
+  {
+    "revision": "1ac5df76d735895ac08a369b4e27cef1",
+    "url": "/index.html"
+  }
+];
